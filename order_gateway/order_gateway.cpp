ec4f676a1807ec9f3dd7b8bd9c67d494b38b6e16
S-Uknown047 <s.knownuser047@gmail.com>
2026-03-24 19:32:36 +0530

    Order gateway Update

--- order_gateway/order_gateway.cpp
+++ order_gateway/order_gateway.cpp
@@ -43,14 +43,14 @@ namespace internal_lib {
 
         uint64_t throttle_cycles = 400;
         
-         inline void busy_spin_throttle () noexcept {
-            if (LIKELY(throttle_cycles > 0)) {
-                uint64_t start = 
-            }
-         }
+        //  inline void busy_spin_throttle () noexcept {
+        //     if (LIKELY(throttle_cycles > 0)) {
+        //         uint64_t start = 
+        //     }
+        //  }
 
-         public: 
-         OrderGateway( Common::LFQueue<internal_lib::LOBOrder>* laq,
+    public: 
+        OrderGateway( Common::LFQueue<internal_lib::LOBOrder>* laq,
         Common::LFQueue<internal_lib::LOBAck>* LOBAck,
         Common::LFQueue<internal_lib::UserOrder>* uo,
         Common::LFQueue<internal_lib::UserAck>* uack,
@@ -66,7 +66,7 @@ namespace internal_lib {
             return -1;
         }
          
-        int AssignSystemId(long long orderId) {
+        int AssignSystemId(uint64_t orderId) {
             int sysId = next_system_id++;
             map.insert({orderId, sysId});
             LUT[sysId] = orderId;
@@ -89,32 +89,140 @@ namespace internal_lib {
             }
 
             while (!terminate_order_gateway.load(std::memory_order_acquire)){
+               // order received at order Gateway from Sniper               
                 UserOrder* readOrder = SniperOrderQueue->getNextToRead();
 
                 if (LIKELY(readOrder != nullptr)) {
                     
                     uint64_t arrival_time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
                     
+
                     auto write_log = Logger->getNextToWriteTo();
 
                     if (write_log != nullptr) {
                         write_log->log_id = 1;
                         write_log->timestamp =  arrival_time;
                         write_log->log_data = *readOrder;
+                        Logger->updateWriteIndex();
+                    }
+                    
+                    int sys_id = AssignSystemId(readOrder->order_id);
+
+                    uint64_t  end_time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+                    
+                    order_gateway_time.push_back(end_time - arrival_time);
+
+                    // creating LOBOrder* request for the SniperOder (userOrder)
+                    
+                    internal_lib::LOBOrder* write =  LobOrderQueue->getNextToWriteTo();
+
+                    if (LIKELY(write != nullptr)) {
+
+                        write->arrival_cycle_count = readOrder->arrived_cycle_count;
+                        write->system_id = sys_id;
+                        write->order_type = readOrder->order_type;
+                        write->price = readOrder->price;
+                        write->quantity = readOrder->quantity;
+                        write->trade_id = readOrder->trader_id;     
+                        write->req_type = readOrder->req_type;
+                        write->out_cycle_count = readOrder->out_cycle_count; 
+
+                        auto lobLog = Logger->getNextToWriteTo();
+
+                        if (LIKELY(write !=  nullptr)) {
+                            
+                            lobLog->log_id = 2;
+                            lobLog->timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+                            lobLog->log_data = *write;
+
+                            Logger->updateWriteIndex();
+                        }
+
+                        LobOrderQueue->updateWriteIndex();
+                        SniperOrderQueue->updateReadIndex();
+
+                    }
+                }
+
+                UserOrder* readOrder = MMorderQueue->getNextToRead();
+
+                
+                if (LIKELY(readOrder != nullptr)) {
+                    LogElement* log = Logger->getNextToWriteTo();
+                    if (LIKELY(log != nullptr)) {
+                        log->log_id = 1;
+                        log->timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+                        log->log_data = *readOrder;
+                        Logger->updateReadIndex();
+                    }
+                    
+                    LOBOrder* write = LobOrderQueue->getNextToWriteTo();
+
+                    if (LIKELY(write != nullptr)) {
+
+                        write->arrival_cycle_count = readOrder->arrived_cycle_count;
+                        write->system_id = AssignSystemId(readOrder->order_id);
+                        write->order_type = readOrder->order_type;
+                        write->price = readOrder->price;
+                        write->quantity = readOrder->quantity;
+                        write->trade_id = readOrder->trader_id;     
+                        write->req_type = readOrder->req_type;
+                        write->out_cycle_count = readOrder->out_cycle_count;
+                        
+                        // order is sent from order Gateway to Matching Engine
+                        LogElement* log = Logger->getNextToWriteTo();
+                        
+                        if (LIKELY(log != nullptr)) {
+                            log->log_id = 2;
+                            log->timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+                            log->log_data = *write;
+                            Logger->updateWriteIndex();
+                        }
+
+                        LobOrderQueue->updateWriteIndex();
+                        MMorderQueue->updateReadIndex();
+                    }
+                }
+
+                // for processing ack  
+
+                // ack received from Matching engine 
+
+                LOBAck* readAck = LobAckQueue->getNextToRead();
+
+                if (LIKELY(readAck != nullptr)) {
+                   
+                    LogElement* logger = Logger->getNextToWriteTo();
+                    if (LIKELY(logger != nullptr)) {
+                        logger->timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock().now().time_since_epoch()).count();
+                        logger->log_id = 9;
+                        logger->log_data = *readAck;
+                    }
+
+                    UserAck* writeAck = SniperOrderAck->getNextToWriteTo();
+
+                    if (LIKELY(writeAck  != nullptr)) {
+                        writeAck->order_id = SystemToOrderId(readAck->system_id);
+                        writeAck->quantity = readAck->quantity;
+                        writeAck->price = readAck->price;
+                        writeAck->side = readAck->side; // Side is buy or sell
+                        writeAck->status = readAck->status;
+
+                        LogElement* logger = Logger->getNextToWriteTo();
+                        if (LIKELY(logger != nullptr)) {
+                            logger->log_id = 10;
+                            logger->timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock().now().time_since_epoch()).count();
+                            logger->log_data = *writeAck;
+                            Logger->updateWriteIndex();
+                        }
+                        
+                        SniperOrderAck->updateWriteIndex();
+                        LobAckQueue->updateReadIndex();
                     }
                 }
             }
 
+            // std::this_thread::sleep_for(std::chrono::seconds(5));
         }
-
-
-
-        
-
-
-
-
-    
-
     };
 };
